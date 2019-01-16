1582f14008bf17c572ec10bf16f595505dbb60de
Piotr Rasała <rasala.piotr@hotmail.com>
2019-01-16 19:44:27 +0100

    Simplified forecast

--- src/Components/Forecast/Forecast.jsx
+++ src/Components/Forecast/Forecast.jsx
@@ -3,6 +3,7 @@ import {bindActionCreators} from "redux";
 import {getWeatherForecast} from "../../Containers/Weather/Weather.action";
 import useRedux from "../../Hooks/redux";
 import Loader from "../Loader/Loader";
+import Entry from "./Entry";
 
 import styles from "./Forecast.module.scss";
 
@@ -40,11 +41,18 @@ const Forecast = () => {
 
     return (
         <div className={styles.forecast}>
-            <span>
-                {
-                    JSON.stringify(forecast)
-                }
-            </span>
+            {
+                forecast.list.map((part, index) => (
+                    <Entry
+                        key={index}
+                        time={part.dt}
+                        temp={part.main.temp}
+                        pressure={part.main.pressure}
+                        humidity={part.main.humidity}
+                        weather={part.weather}
+                    />)
+                )
+            }
         </div>
     )
 };
